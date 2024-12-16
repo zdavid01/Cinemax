39703155f7d77cc7a345bafff342956b44451f00
zdavid01 <dzivkovicd1@gmail.com>
2024-12-16 15:27:16 +0100

    Add factories

---
+++ Cinemax/Payment.Infrastructure/Factories/PaymentViewModelFactory.cs
@@ -0,0 +1,34 @@
+using Payment.Application.Factories;
+using Payment.Application.Features.Payments.Queries.ViewModels;
+using Payment.Domain.Entities;
+
+namespace Payment.Infrastructure.Factories;
+
+public class PaymentViewModelFactory : IPaymentViewModelFactory
+{
+    public PaymentViewModel CreateViewModel(Domain.Aggregates.Payment payment)
+    {
+        var paymentVM = new PaymentViewModel();
+        paymentVM.Id = payment.Id;
+        paymentVM.BuyerId = payment.BuyerId;
+        paymentVM.BuyerUsername = payment.BuyerUsername;
+        paymentVM.Amount = payment.Money.Amount;
+        paymentVM.Currency = payment.Money.Currency;
+        
+        var paymentItems = new List<PaymentItemViewModel>();
+
+        foreach (var item in payment.PaymentItems)
+        {
+            var paymentItem = new PaymentItemViewModel();
+            paymentItem.Id = item.Id;
+            paymentItem.MovieName = item.MovieName;
+            paymentItem.MovieId = item.MovieId;
+            paymentItem.Price = item.Price;
+            paymentItem.Quantity = item.Quantity;
+            paymentItems.Add(paymentItem);
+        }
+        paymentVM.PaymentItems = paymentItems;
+        
+        return paymentVM;
+    }
+}
